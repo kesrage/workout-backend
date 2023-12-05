af301d93c8bb8efa2702b86d75929b608b4d6636
Eugene Kukarin <e.kukarin@gmail.com>
2023-12-06 00:39:14 +0400

    Добавление лога тренировок и упражнений

--- app/exercise/exersice.routes.js
+++ app/exercise/exersice.routes.js
@@ -6,10 +6,16 @@ import {
 	getExercises,
 	updateExercise
 } from './exersice.controller.js' //Импорт контроллера AuthUser
+import { createNewExerciseLog } from './log/exercise-log.controller.js'
 
 const router = express.Router()
 router.route('/').post(protect, createNewExercise).get(protect, getExercises)
 
-router.route('/:id').put(protect, updateExercise).delete(protect, deleteExercise)
+router
+	.route('/:id')
+	.put(protect, updateExercise)
+	.delete(protect, deleteExercise)
+
+router.route('/log/:exerciseId').post(protect, createNewExerciseLog)
 
 export default router
---
+++ app/exercise/log/exercise-log.controller.js
@@ -0,0 +1,55 @@
+//@desc Create new exerciseLog
+//@route POST /api/exercises/log/:exerciseId
+//@access Private
+
+import { prisma } from '../../prisma.js'
+import asyncHandler from 'express-async-handler'
+
+export const createNewExerciseLog = asyncHandler(async (req, res) => {
+
+	const exerciseId = +req.params.exerciseId
+
+    const exercise = await prisma.exercise.findUnique({
+        where: {
+            id: exerciseId
+        }
+    })
+
+    if(!exercise) {
+        res.status(404)
+        throw new Error('Exercise not found!')
+    }
+
+	let timesDefault = []
+
+	for (let i = 0; i < exercise.times; i++) {
+		timesDefault.push({
+			weight: 0,
+			repeat: 0
+		})
+	}
+
+	const exerciseLog = await prisma.exerciseLog.create({
+		data: {
+			user: {
+				connect: {
+					id: req.user.id
+				}
+			},
+			exercise: {
+				connect: {
+					id: exerciseId
+				}
+			},
+			times: {
+				createMany: {
+					data: timesDefault
+				}
+			}
+		},
+		include: {
+			times: true
+		}
+	})
+	res.json(exerciseLog)
+})
